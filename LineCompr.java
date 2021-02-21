e6954238942fb5f6192a974b37c697f489830823
unknown <kumar.skm03@gmail.com>
2021-02-21 17:14:28 +0530

    added compareTo method in LineCompr.java

--- LineCompr.java
+++ LineCompr.java
@@ -17,20 +17,22 @@ public class LineCompr {
 		for (int i=0; i < 2; i++)
 		{
 			System.out.println("Enter cordinates for Line" + (i+1));
-			System.out.println("Start points: ");
+			System.out.println("Start points (x1,y1): ");
 			x1 = sc.nextInt();
 			y1 = sc.nextInt();
-			System.out.println("End points: ");
+			System.out.println("End points (x2,y2): ");
 			x2 = sc.nextInt();
 			y2 = sc.nextInt();
 
 			length[i] = lengthCalcu(x1, y1, x2, y2);
 		}
 
-		if (length[0].equals(length[1]))
+		int check = length[0].compareTo(length[1]); //compareTo
+		if (check == 0)
 			System.out.println("Lines are equal");
+		else if (check > 0)
+			System.out.println("Line1 is larger than line2");
 		else
-			System.out.println("Lines are not equal");
-
+			System.out.println("Line2 is larger than line1");
 	}
 }
